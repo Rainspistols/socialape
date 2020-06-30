8fd7839adc8a23af90363dd17afe3019e668d5ec
Rainspistols <ffly@mail.ru>
2020-06-30 16:52:48 +0200

    Express & formating response

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is a scream body',
+      createdAt: '2020-06-30T08:53:50.545Z',
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
